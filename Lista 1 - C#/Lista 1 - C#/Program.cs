efa66cdb94c0a4435f5e35b77813a474342f83a4
Henrique Pereira <133662766+HenriqueP21@users.noreply.github.com>
2024-04-17 16:55:44 -0300

    Add files via upload

---
+++ Lista 1 - C#/Lista 1 - C#/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            /*Atividade 1
+
+            int horas = 8;
+            int salario = 35;
+
+            int multiplicacao = salario * horas;
+            Console.WriteLine("Seu salário em um dia será " + multiplicacao + " reais.");*/
+
+            /*Atividade 2
+
+            int caixa = 27;
+            int bombom = 16;
+
+            int bombons = caixa * bombom;
+
+            Console.WriteLine("Pamela terá " + bombons + " bombons");*/
+
+            /*Atividade 3
+
+            int valorUm = 5;
+            int valorDois = 36;
+
+            int soma = valorDois + valorUm;
+
+            Console.WriteLine("A soma dos dois valores lidos é " + soma);*/
+
+            /*Atividade 4
+
+            int garrafas = 300;
+            int caixas = 20;
+
+            int divisao = garrafas / caixas;
+            Console.WriteLine("Eles usaram " + divisao + " para armazenar 300 garrafas.");*/
+
+            /* Atividade 5
+
+            int instalacao = 300;
+            int limpeza = 150;
+            double manutencao = 499.99;
+            int desconto = 50;
+
+            double soma = instalacao + limpeza + manutencao - desconto;
+            Console.WriteLine("Novos clientes deveram pagar para a empresa R$ " + soma);*/
+
+
+
+
+        }
+    }
+}
